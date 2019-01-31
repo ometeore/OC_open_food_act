d5a7a9dd92d5224f8b657208b8cda6b9e7e83de5
PILT <pilt64@hotmail.fr>
2019-01-31 16:46:30 +0100

    debut de projet ca pars dans tous les sens

---
+++ api_sql.py
@@ -0,0 +1,66 @@
+#####################################################################################
+############################UTILISATION DE OPEN FOOD ACTS############################
+#####################################################################################
+
+
+import json, MySQLdb, requests
+    
+db = MySQLdb.connect(host="localhost",
+    user="root",
+     passwd="Vendetta7800",      # <--------- vive la discression A metre dans un autre fichier bixon
+    db="OpenClassroom")
+#on doit créer un objet curseur??
+
+
+
+cur = db.cursor()
+
+cur.execute("SELECT * FROM open_food_act")
+
+for row in cur.fetchall():
+    print(row)
+ 
+
+db.close()
+#r = requests.get("https://world.openfoodfacts.org/api/v0/product/3392460511200.json")
+ 
+#r -> json  // result -> dictionnaire
+#result = json.loads(r.text)
+#req = requests.request('GET', 'https://world.openfoodfacts.org/api/v0/product/3392460511200.json')
+#print(req)
+
+#affiche tout en vrac si décommenté
+#result1 = result[product][categories_prev_hierarchy]=en:sugary-snacks
+#print(result1)
+#print(result)
+ 
+
+#url="https://world.openfoodfacts.org/api/v0/product/%s.json"(*)
+#url="https://fr.openfoodfacts.org/categories.json"
+#content=requests.get(url)
+#data=content.json()
+#t=data['tags']
+#print(t[0:20])
+#baseDonn = gadfly.gadfly("","")
+
+if __name__ == '__main__':
+    def __main__():
+        """ Ceci est le main"""
+        continuer_choix = True
+        while continuer_choix:
+            print("****")
+            print("****")
+            print("****")
+            print("1. Quel aliment souhaitez vous remplacer?")
+            print("2. Retrouvez les aliments que vous souhaitez substituer.")
+            continuer_choix = input("             Que voulez vous faire?")
+            if continuer_choix == "1":
+                print("****") 
+                print("Sélectionnez la catégorie")
+                print("****") 
+            else:
+                print("****") 
+                print("coucou maman :).")
+                print("****") 
+
+    __main__()
